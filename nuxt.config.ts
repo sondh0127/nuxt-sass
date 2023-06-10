3e728b6d603525ec551bb1e22c9eca8bec8c3707
Son Hong Do <sondh0127@gmail.com>
2023-06-10 23:29:37 +0700

    runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -109,5 +109,16 @@ export default defineNuxtConfig({
   components: [
     { path: '~/components', extensions: ['vue'] },
   ],
-
+  runtimeConfig: {
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    stripeEndpointSecret: process.env.STRIPE_ENDPOINT_SECRET,
+    subscriptionGraceDays: 3,
+    initialPlanName: 'Free Trial',
+    initialPlanActiveMonths: 1,
+    openAIKey: process.env.OPENAI_API_KEY,
+    public: {
+      debugMode: true,
+      siteRootUrl: process.env.URL || 'http://localhost:3000', // URL env variable is provided by netlify by default
+    },
+  },
 })
