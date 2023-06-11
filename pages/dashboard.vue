e4faee5850e3d088aceb1aa8aa3a8cf6a7de123c
Son Hong Do <sondh0127@gmail.com>
2023-06-11 15:58:27 +0700

    sidebar

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -21,108 +21,99 @@ const { $client } = useNuxtApp()
 <template>
   <el-container class="layout-container-demo h-full w-full">
     <el-aside class="border-r border-$el-border-color" width="256px">
-      <el-scrollbar>
-        <div class="flex items-center justify-center gap-3">
-          <el-icon i-logos:nuxt-icon size="40" />
-          <span>
-            NUXT SASS
-          </span>
+      <el-scrollbar class="[&_.el-scrollbar\_\_view]:h-full">
+        <div class="h-full flex flex-col">
+          <div class="flex items-center justify-center gap-3">
+            <el-icon i-logos:nuxt-icon size="40" />
+            <span>
+              NUXT SASS
+            </span>
+          </div>
+
+          <el-menu class="flex-1 !border-r-none" :default-openeds="['1', '3']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon>
+                  <Message />
+                </el-icon>Navigator One
+              </template>
+
+              <el-sub-menu index="1-4">
+                <template #title>
+                  Option4
+                </template>
+                <el-menu-item index="1-4-1">
+                  Option 4-1
+                </el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+          </el-menu>
+
+          <el-menu class="!border-r-none" :default-openeds="['1', '3']">
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon>
+                  <Message />
+                </el-icon>Navigator One
+              </template>
+              <el-menu-item-group>
+                <template #title>
+                  Group 1
+                </template>
+                <el-menu-item index="1-1">
+                  Option 1
+                </el-menu-item>
+                <el-menu-item index="1-2">
+                  Option 2
+                </el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="1-3">
+                  Option 3
+                </el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="1-4">
+                <template #title>
+                  Option4
+                </template>
+                <el-menu-item index="1-4-1">
+                  Option 4-1
+                </el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+            <el-sub-menu index="2">
+              <template #title>
+                <el-icon>
+                  <IconMenu />
+                </el-icon>Navigator Two
+              </template>
+              <el-menu-item-group>
+                <template #title>
+                  Group 1
+                </template>
+                <el-menu-item index="2-1">
+                  Option 1
+                </el-menu-item>
+                <el-menu-item index="2-2">
+                  Option 2
+                </el-menu-item>
+              </el-menu-item-group>
+              <el-menu-item-group title="Group 2">
+                <el-menu-item index="2-3">
+                  Option 3
+                </el-menu-item>
+              </el-menu-item-group>
+              <el-sub-menu index="2-4">
+                <template #title>
+                  Option 4
+                </template>
+                <el-menu-item index="2-4-1">
+                  Option 4-1
+                </el-menu-item>
+              </el-sub-menu>
+            </el-sub-menu>
+          </el-menu>
         </div>
-        <el-menu class="!border-r-none" :default-openeds="['1', '3']">
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon>
-                <Message />
-              </el-icon>Navigator One
-            </template>
-            <el-menu-item-group>
-              <template #title>
-                Group 1
-              </template>
-              <el-menu-item index="1-1">
-                Option 1
-              </el-menu-item>
-              <el-menu-item index="1-2">
-                Option 2
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="1-3">
-                Option 3
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="1-4">
-              <template #title>
-                Option4
-              </template>
-              <el-menu-item index="1-4-1">
-                Option 4-1
-              </el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="2">
-            <template #title>
-              <el-icon>
-                <IconMenu />
-              </el-icon>Navigator Two
-            </template>
-            <el-menu-item-group>
-              <template #title>
-                Group 1
-              </template>
-              <el-menu-item index="2-1">
-                Option 1
-              </el-menu-item>
-              <el-menu-item index="2-2">
-                Option 2
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="2-3">
-                Option 3
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="2-4">
-              <template #title>
-                Option 4
-              </template>
-              <el-menu-item index="2-4-1">
-                Option 4-1
-              </el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-          <el-sub-menu index="3">
-            <template #title>
-              <el-icon>
-                <Setting />
-              </el-icon>Navigator Three
-            </template>
-            <el-menu-item-group>
-              <template #title>
-                Group 1
-              </template>
-              <el-menu-item index="3-1">
-                Option 1
-              </el-menu-item>
-              <el-menu-item index="3-2">
-                Option 2
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="Group 2">
-              <el-menu-item index="3-3">
-                Option 3
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-sub-menu index="3-4">
-              <template #title>
-                Option 4
-              </template>
-              <el-menu-item index="3-4-1">
-                Option 4-1
-              </el-menu-item>
-            </el-sub-menu>
-          </el-sub-menu>
-        </el-menu>
       </el-scrollbar>
     </el-aside>
 
