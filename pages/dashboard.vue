7bd79ecfc7dc7e272393a02e1ea621246ed25f8e
Son Hong Do <sondh0127@gmail.com>
2023-07-05 23:41:32 +0700

    drizzle

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,10 +3,16 @@ definePageMeta({
   layout: 'dashboard',
   middleware: ['auth'],
 })
+const { $client } = useNuxtApp()
+const { data } = $client.auth.getAllUser.useQuery()
 </script>
 
 <template>
   <div>
     dashboard
+    <details open>
+      <summary>data</summary>
+      <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+    </details>
   </div>
 </template>
--- server/trpc/routers/auth.router.ts
+++ server/trpc/routers/auth.router.ts
@@ -1,3 +1,5 @@
+import { db } from '../../../db/client'
+import { users } from '../../../db/schema'
 import { publicProcedure, router } from '../trpc'
 
 export const authRouter = router({
@@ -7,4 +9,7 @@ export const authRouter = router({
         dbUser: ctx.dbUser,
       }
     }),
+  getAllUser: publicProcedure.query(({ ctx }) => {
+    return db.select().from(users)
+  }),
 })
