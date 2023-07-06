91d26c9b38d8ffe83366ff6a5e6f7a4d7491ba3b
Son Hong Do <sondh0127@gmail.com>
2023-07-06 08:05:25 +0700

    drizzle

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,18 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core'
+import { integer, pgTable, serial, text, uniqueIndex, varchar } from 'drizzle-orm/pg-core'
+
+export const plan = pgTable('plan', {
+  id: serial('id').primaryKey().notNull(),
+  name: text('name').notNull(),
+  features: text('features').array(),
+  maxNotes: integer('max_notes').default(100).notNull(),
+  stripeProductId: text('stripe_product_id'),
+  maxMembers: integer('max_members').default(1).notNull(),
+},
+(table) => {
+  return {
+    nameKey: uniqueIndex('plan_name_key').on(table.name),
+  }
+})
 
 export const users = pgTable('users', {
   id: serial('id').notNull(),
--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -53,24 +53,3 @@ export const token = pgTable('token', {
   accountId: integer('account_id').references(() => account.id, { onDelete: 'set null', onUpdate: 'cascade' }),
   tokenText: text('token_text').notNull(),
 })
-
-export const plan = pgTable('plan', {
-  id: serial('id').primaryKey().notNull(),
-  name: text('name').notNull(),
-  features: text('features').array(),
-  maxNotes: integer('max_notes').default(100).notNull(),
-  stripeProductId: text('stripe_product_id'),
-  maxMembers: integer('max_members').default(1).notNull(),
-},
-(table) => {
-  return {
-    nameKey: uniqueIndex('plan_name_key').on(table.name),
-  }
-})
-
-export const users = pgTable('users', {
-  id: serial('id').notNull(),
-  supabaseUid: text('supabase_uid').notNull(),
-  email: text('email'),
-  displayName: text('display_name'),
-})
