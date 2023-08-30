78487cf40c7711116ca7eb134d7b1c588cdc332a
Son Hong Do <sondh0127@gmail.com>
2023-08-30 09:18:42 +0700

    lock refresh

--- pages/account.vue
+++ pages/account.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-import { ACCOUNT_ACCESS } from '~/prisma/account-access-enum'
+
+// import { ACCOUNT_ACCESS } from '~/prisma/account-access-enum'
 
 definePageMeta({
   layout: 'dashboard',
