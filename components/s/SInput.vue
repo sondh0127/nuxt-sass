89242d49b1d782089ee199990a7c24eacbea22a1
Son Hong Do <sondh0127@gmail.com>
2023-10-21 23:30:08 +0700

    rule

---
+++ components/s/SInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useVModel } from '@vueuse/core'
+
+const props = defineProps<{
+  defaultValue?: string | number
+  modelValue?: string | number
+}>()
+
+const emits = defineEmits<{
+  (e: 'update:modelValue', payload: string | number): void
+}>()
+
+const modelValue = useVModel(props, 'modelValue', emits, {
+  passive: true,
+  defaultValue: props.defaultValue,
+})
+</script>
+
+<template>
+  <input v-model="modelValue" type="text" :class="cn('flex h-44px w-full rounded-8px bg-[rgba(0,0,0,0.30)] px-3 py-2 text-cap ring-offset-[rgba(17,17,17,0.30)] file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:font-400 placeholder:text-[rgba(255,255,255,0.50)] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-blue-500/50 focus-visible:ring-offset-1 text-white disabled:cursor-not-allowed disabled:opacity-50 font-400', $attrs.class ?? '')">
+</template>
--- uno.config.ts
+++ uno.config.ts
@@ -7,7 +7,10 @@ import {
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
-import { grass, green, mauve } from '@radix-ui/colors'
+import { presetTheme } from './uno/theme.preset.ts'
+import { sizeRules } from './uno/size.rule.ts'
+
+// SECTION
 
 export default defineConfig({
   presets: [
@@ -17,16 +20,66 @@ export default defineConfig({
       scale: 1.2,
     }),
     presetTypography(),
+    presetTheme({
+      theme: {
+        dark: {
+          colors: {
+            border: 'hsl(214.3,31.8%,91.4%)',
+            input: 'hsl(214.3,31.8%,91.4%)',
+            ring: 'hsl(221.2,83.2%,53.3%)',
+            background: 'hsl(0,0%,100%)',
+            foreground: 'hsl(222.2,84%,4.9%)',
+            primary: {
+              DEFAULT: 'hsl(221.2,83.2%,53.3%)',
+              foreground: 'hsl(210,40%,98%)',
+            },
+            secondary: {
+              DEFAULT: 'hsl(210,40%,96.1%)',
+              foreground: 'hsl(222.2,47.4%,11.2%)',
+            },
+            destructive: {
+              DEFAULT: 'hsl(0,84.2%,60.2%)',
+              foreground: 'hsl(210,40%,98%)',
+            },
+            muted: {
+              DEFAULT: 'hsl(var(--muted))',
+              foreground: 'hsl(var(--muted-foreground))',
+            },
+            accent: {
+              DEFAULT: 'hsl(var(--accent))',
+              foreground: 'hsl(var(--accent-foreground))',
+            },
+            popover: {
+              DEFAULT: 'hsl(var(--popover))',
+              foreground: 'hsl(var(--popover-foreground))',
+            },
+            card: {
+              DEFAULT: 'hsl(0,0%,100%)',
+              foreground: 'hsl(222.2,84%,4.9%)',
+            },
+          },
+        },
+        light: {
+        },
+      },
+    }),
   ],
   transformers: [
     transformerDirectives(),
     transformerVariantGroup(),
   ],
+  rules: [
+    ...sizeRules,
+  ],
   theme: {
-    colors: {
-      ...mauve,
-      ...green,
-      ...grass,
+    borderRadius: {
+      lg: 'var(--radius)',
+      md: 'calc(var(--radius) - 2px)',
+      sm: 'calc(var(--radius) - 4px)',
+    },
+    boxShadow: {
+      switch:
+        'rgba(0, 0, 0, 0.3) 0px 0px 1px, rgba(0, 0, 0, 0.2) 0px 1px 2px',
     },
     animation: {
       keyframes: {
@@ -40,6 +93,13 @@ export default defineConfig({
       timingFns: {
         slideDown: 'cubic-bezier(0.87, 0, 0.13, 1)',
         slideUp: 'cubic-bezier(0.87, 0, 0.13, 1)',
+      },
+    },
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
       },
     },
   },
