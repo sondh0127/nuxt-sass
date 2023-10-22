96e9a63ee8e5b0efa4279951d45fbd00d5796981
Son Hong Do <sondh0127@gmail.com>
2023-10-22 12:56:53 +0700

    studio

--- server/api/todo.post.ts
+++ server/api/todo.post.ts
@@ -1,4 +1,9 @@
 export default defineEventHandler(async (event) => {
-  const body = await readValidatedBody(event, InsertSchemaTodo.parse) // or `.parse` to throw an error
-  return db.insert(todo).values(body)
+  try {
+    const body = await readValidatedBody(event, InsertSchemaTodo.parse) // or `.parse` to throw an error
+    return db.insert(todo).values(body)
+  }
+  catch (error) {
+    console.log('[LOG] ~ file: todo.post.ts:6 ~ error:', error)
+  }
 })
