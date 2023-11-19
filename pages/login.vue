b5ac936f3f2fd2f64fb04b79e2ce299d572de6cc
Son Hong Do <sondh0127@gmail.com>
2023-11-19 19:02:53 +0700

    new supa

--- pages/login.vue
+++ pages/login.vue
@@ -1,107 +1,25 @@
 <script setup lang="ts">
-import { destr } from 'destr'
+const supabase = useSupabaseClient()
+const email = ref('')
 
-// const user = useSupabaseUser()
-// watchEffect(() => {
-//   if (user.value)
-//     navigateTo('/dashboard', { replace: true })
-//   // else
-//   //   navigateTo('/signin', { replace: true })
-// })
-
-// const accordionItems = [
-//   {
-//     value: 'item-1',
-//     title: 'Is it accessible?',
-//     content: 'Yes. It adheres to the WAI-ARIA design pattern.',
-//   },
-//   {
-//     value: 'item-2',
-//     title: 'Is it unstyled?',
-//     content: 'Yes. It\'s unstyled by default, giving you freedom over the look and feel.',
-//   },
-//   {
-//     value: 'item-3',
-//     title: 'Can it be animated?',
-//     content: 'Yes! You can use the transition prop to configure the animation.',
-//   },
-// ]
-
-const { data } = await useFetch('/api/todo', { key: '/api/todo' })
-
-const text = ref()
-const editting = ref<EditSchema>()
-const error = ref([])
-
-async function createTodo() {
-  try {
-    const res = await $fetch('/api/todo', { method: 'POST', body: { text: text.value } })
-    refreshNuxtData('/api/todo')
-    text.value = undefined
-  }
-  catch (err) {
-    error.value = destr(err.data.message)
-  }
-}
-
-function editTodo(item: EditSchema) {
-  editting.value = klona(item)
-}
-
-async function submitEdit() {
-  const { text, id } = editting.value
-  await $fetch(`/api/todo/${id}`, { method: 'PATCH', body: { text } })
-  refreshNuxtData('/api/todo')
-  editting.value = undefined
-}
-
-async function deleteTodo(id: number) {
-  await $fetch(`/api/todo/${id}`, { method: 'DELETE' })
-  refreshNuxtData('/api/todo')
+const signInWithOtp = async () => {
+  const { error } = await supabase.auth.signInWithOtp({
+    email: email.value,
+    options: {
+      emailRedirectTo: 'http://localhost:3000/confirm',
+    }
+  })
+  if (error) console.log(error)
 }
 </script>
-
 <template>
   <div>
-    Index
-    <DevOnly>
-      <details open>
-        <summary>data</summary>
-        <pre>{{ JSON.stringify(data, null, 2) }}</pre>
-      </details>
-    </DevOnly>
-    <div>
-      <div v-for="item in data" :key="item.id">
-        <div v-if="editting?.id === item.id" class="flex">
-          <SInput v-model="editting.text" />
-          <SButton @click="submitEdit">
-            Submit
-          </SButton>
-        </div>
-
-        <div v-else class="flex">
-          <p>
-            {{ item.text }}
-          </p>
-          <SButton @click="editTodo(item)">
-            Edit
-          </SButton>
-          <SButton variant="destructive" @click="deleteTodo(item.id)">
-            Delete
-          </SButton>
-        </div>
-
-        <p>Done: {{ item.done }}</p>
-      </div>
-    </div>
-    <SInput v-model="text" />
-    <SButton @click="createTodo">
-      Create
-    </SButton>
-    <DevOnly>
-      <details open>
-        <VueJsonPretty :data="error" />
-      </details>
-    </DevOnly>
+    <button @click="signInWithOtp">
+      Sign In with E-Mail
+    </button>
+    <input
+      v-model="email"
+      type="email"
+    />
   </div>
 </template>
