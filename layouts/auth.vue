0a160f5c0102b9ddc03794e7515dd55447f4ab2d
Son Hong Do <sondh0127@gmail.com>
2023-12-23 09:42:33 +0700

    layout

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -8,25 +8,16 @@
   >
     <div class="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
       <div class="absolute inset-0 from-primary to-primary/90 bg-gradient-to-r" />
-      <div class="relative z-20 flex items-center text-lg font-medium">
-        <svg
-          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
-          strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-6 w-6"
-        >
-          <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-        </svg>
-        Acme Inc
+      <div class="relative z-20 flex items-center gap-2 text-lg font-medium">
+        <div i-ri:book-3-line />
+        SavingBook
       </div>
-      <div class="relative z-20 mt-auto">
-        <blockquote class="space-y-2">
+      <div class="relative z-20 mt-auto flex items-center gap-2">
+        <DarkToggle />
+        <blockquote>
           <p class="text-lg">
-            &ldquo;This library has saved me countless hours of work and
-            helped me deliver stunning designs to my clients faster than
-            ever before.&rdquo;
+            &ldquo;A good book is the best of friends, the same today and forever.&rdquo;
           </p>
-          <footer class="text-sm">
-            Sofia Davis
-          </footer>
         </blockquote>
       </div>
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,5 @@
 </script>
 
 <template>
-  <header class="h-60px flex items-center justify-end gap-4 px-6">
-    <DarkToggle />
-  </header>
-  <main class="h-[calc(100dvh-60px)]">
-    <slot />
-  </main>
+  <slot />
 </template>
