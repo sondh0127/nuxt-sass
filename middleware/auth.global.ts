81ea514c8eb6da8c7dcbcfabd61b521a3e0dfdd7
Son Hong Do <sondh0127@gmail.com>
2024-01-28 01:30:35 +0700

    remove log

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,6 +2,4 @@ export default defineNuxtRouteMiddleware(async () => {
   const user = useUser()
 
   user.value = await $fetch('/api/user')
-  if (import.meta.server)
-    console.log(user.value)
 })
