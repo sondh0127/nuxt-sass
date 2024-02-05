db0e5589edd86915338f236210d8c72e0d68c6d5
Son Hong Do <sondh0127@gmail.com>
2024-02-06 02:58:43 +0700

    build

--- server/routes/login/github/index.get.ts
+++ server/routes/login/github/index.get.ts
@@ -6,7 +6,9 @@ export default defineEventHandler(async (event) => {
 
   setCookie(event, 'github_oauth_state', state, {
     path: '/',
-    secure: process.env.NODE_ENV === 'production',
+    // TODO:  change this when https
+    secure: false,
+    // secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     maxAge: 60 * 10,
     sameSite: 'lax',
