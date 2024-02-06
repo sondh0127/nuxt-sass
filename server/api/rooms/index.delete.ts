571cde26e5264ecac36ecdab16c9a95c9a1866f7
Son Hong Do <sondh0127@gmail.com>
2024-02-06 08:40:25 +0700

    fix db

--- server/api/rooms/index.delete.ts
+++ server/api/rooms/index.delete.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const { id } = await readBody(event)
-
+  const db = useDb()
   await db.delete(RoomTable)
     .where(eq(RoomTable.id, id))
     .returning()
